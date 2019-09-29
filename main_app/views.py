41687d53c664550521cfe8d624b2ae1bc0c537f7
Dev Govindji <friendsofdev94@gmail.com>
2019-09-29 18:27:56 -0500

    create basic timer to learn functionality

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import time
 
 # Create your views here.
 
 def home(request):
     return render(request, 'about.html')
+
+def countdown(n) :
+    while n > 0:
+        print (n)
+        n = n - 1
+        if n ==0:
+            print('BLAST OFF!')
+
+
+countdown(50)
