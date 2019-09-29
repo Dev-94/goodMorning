ee05511e66df4b9f0482784f0e3fff9adf58136f
Dev Govindji <friendsofdev94@gmail.com>
2019-09-29 18:46:59 -0500

    implements threading timer functionality for testing purposes

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import time
+import threading
 
 # Create your views here.
 
@@ -15,4 +16,17 @@ def countdown(n) :
             print('BLAST OFF!')
 
 
-countdown(50)
+def gfg():
+    print("good morning\n")
+
+timer = threading.Timer(10.0, gfg)
+timer.start()
+print('good night\n')
+
+countdown(5)
+
+
+
+# calculate 24 hours into seconds
+# learn how to call twilio into app
+    # insert twilio call into timer.start()
