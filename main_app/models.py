dd2468944c126d55eac04a65cdc7da47da5cea09
Dev Govindji <friendsofdev94@gmail.com>
2019-09-29 19:05:28 -0500

    establish model boilerplate

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+# class Question(model.Models):
+#     question = models.CharField(max_length= 100)
+
+# class User(models.Models):
+#     name = models.CharField(max_length= 100)
+#     number = models.CharField(max_length= 20)
